726661d89a4b7a920829c680c8fea3d2ef4adf88
yanchilei <yanchilei@163.com>
2023-09-13 14:14:21 +0800

    feat: complete i18n

--- app.js
+++ app.js
@@ -20,21 +20,43 @@ function onProceed () {
     throw new Error('No host or href');
   }
 
-  console.log({
-    hash,
-    hashContents,
-    scamHostname,
-    scamHref,
-  })
-
   sendDocumentMessage('SCAM_WARNING_PROCEED', { scamHostname, scamHref, timestamp: Date.now() });
 
   window.location.href = scamHref;
 }
 
-window.onload = function () {
-  document.querySelector('#proceed-btn').addEventListener('click', onProceed);
+function getQueryVariable(variable) {
+    const query = window.location.search.substring(1);
+    const vars = query.split("&");
+    for (let i=0;i<vars.length;i++) {
+        const pair = vars[i].split("=");
+        if(pair[0] == variable){return pair[1];}
+    }
+    return;
+}
 
+function renderTexts(lang) {
+  const langs = ['en', 'ja', 'vi', 'id', 'bd', 'zh-CN'];
+  if (!langs.includes(lang)) {
+    lang = 'en';
+  }
+  const keys = Object.keys(i18n);
+  for (let i = 0; i < keys.length; i++) {
+    const el = document.querySelector(`#${keys[i]}`);
+    if (el) {
+      el.innerText = i18n[keys[i]][lang] || i18n[keys[i]]['en'] || '';
+    }
+  }
+}
+
+
+window.onload = function () {
+  const { hash } = new URL(window.location.href);
+  const hashContents = hash.slice(1);
+  const hashQueryString = new URLSearchParams(hashContents);
+  const lang = hashQueryString.get('lang') || navigator.language;
+  renderTexts(lang);
+  document.querySelector('#proceed-btn').addEventListener('click', onProceed);
 }
 
 window.onunload = function () {
